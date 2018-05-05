1b998a45343c8e03a424b8d70e614419195ff080
islam jameel <islam.jameel9@gmail.com>
2018-05-05 11:19:45 +0300

    make it better

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2018_05_05_075656) do
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "body"
     t.boolean "status"
